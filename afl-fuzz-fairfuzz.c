020c2977260c26321c50c158ad9f313c97ed4aa0
Caroline Lemieux <caro.lemieux@gmail.com>
2019-01-18 16:45:24 -0800

    Add patch for the case where we haven't found a path yet

--- afl-fuzz-fairfuzz.c
+++ afl-fuzz-fairfuzz.c
@@ -9143,7 +9143,9 @@ int main(int argc, char** argv) {
 
       // Stopping more eagerly than a regular fuzzing run.
     if (sv_test_comp) {
-      u64 min_wo_finds = (get_cur_time() - last_path_time) / 1000 / 60;
+      u64 min_wo_finds = 0;
+      if (last_path_time > 0)
+         min_wo_finds = (get_cur_time() - last_path_time) / 1000 / 60;
       if ((cycles_wo_finds > 10 && !pending_not_fuzzed) || cycles_wo_finds > 100 || min_wo_finds > 10)
         stop_soon = 1;
     }
